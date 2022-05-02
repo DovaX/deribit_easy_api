0aa7be3c66e912055239e2f076c72ca4445addd4
Dominik Vach <vach.dominik@gmail.com>
2022-05-02 20:05:05 +0200

    small refactor + api calls for cancel, cancel_all, edit, get_open_orders, get_positions

--- deribit_easy_api_core.py
+++ deribit_easy_api_core.py
@@ -7,25 +7,31 @@ from datetime import datetime
 import nest_asyncio
 nest_asyncio.apply()
 
-async def call_api(msg):
-    async with websockets.connect('wss://test.deribit.com/ws/api/v2') as websocket:
+
+async def call_api(msg,is_testnet=False):
+    if is_testnet:
+        url='wss://test.deribit.com/ws/api/v2'
+    else:
+        url='wss://www.deribit.com/ws/api/v2'
+    async with websockets.connect(url) as websocket:
         await websocket.send(msg)
         while websocket.open:
             response = await websocket.recv()
             json_par = json.loads(response)
             return(json_par)
  
-def run_async_call(msg):
+def run_async_call(msg,is_testnet=False):
     try:
-        response = asyncio.get_event_loop().run_until_complete(call_api(json.dumps(msg)))
+        response = asyncio.get_event_loop().run_until_complete(call_api(json.dumps(msg),is_testnet))
     except Exception as e:
         print("Error while accessing API",e)
     return(response)     
 
 class DeribitClient:
-    def __init__(self,client_id,client_secret):
+    def __init__(self,client_id,client_secret,is_testnet=False):
         self.client_id = client_id
         self.client_secret = client_secret
+        self.is_testnet=is_testnet
         auth=self.authenticate()
         self.access_token=auth["result"]["access_token"]
         
@@ -38,8 +44,7 @@ class DeribitClient:
             msg=bytes('{}\n{}\n{}'.format(timestamp, nonce, data), "latin-1"),
             digestmod=hashlib.sha256
         ).hexdigest().lower()
-        params={
-            
+        params={ 
             "grant_type" : "client_signature",
             "client_id" : self.client_id,
             "client_secret" : self.client_secret,
@@ -49,20 +54,17 @@ class DeribitClient:
             "data" : data,
             "scope":"trade:read_write session:mysessionname"
           }
-        
         response=self.request("public/auth",params=params)
         return(response)
 
-        
     def request(self,method,params):
-        
         msg = {
           "jsonrpc" : "2.0",
           "id" : 1,
           "method" : method,
           "params" : params
         }
-        response=run_async_call(msg) 
+        response=run_async_call(msg,self.is_testnet) 
         return(response)
 
 
@@ -115,5 +117,43 @@ class DeribitClient:
           }
         response=self.request("private/sell",params)
         return(response)
+    
+    def cancel(self, order_id):
+        params = {
+            "access_token" : self.access_token,
+            "order_id": order_id
+        }  
+
+        return self.request("private/cancel", params)
+
+
+    def cancel_all(self):
+        params={"access_token" : self.access_token,
+            }
+        return self.request("private/cancel_all", params)
+
+
+    def edit(self, order_id, price, amount):
+        params = {
+        "access_token" : self.access_token,
+        "order_id" : order_id,
+        "amount" : amount,
+        "price" : price
+        }
+        return self.request("private/edit", params)
+
+
+    def get_open_orders(self, instrument="BTC-PERPETUAL"):
+        params = {"access_token" : self.access_token,
+                  "instrument_name":instrument,
+                  }
+        return self.request("private/get_open_orders_by_instrument", params)
+
+
+    def get_positions(self, instrument="BTC-PERPETUAL"):
+        params = {"access_token" : self.access_token,
+                  "instrument_name":instrument,
+                  }
+        return self.request("private/get_position", params)
 
 
